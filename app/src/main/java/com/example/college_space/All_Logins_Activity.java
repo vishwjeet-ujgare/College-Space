4be4d0e9cd90e6bebf19e68e162c8da611dbc46a
vishwjeet <vrvishwujgare@gmail.com>
2022-12-17 12:11:20 +0530

    Registration Design completed

--- app/src/main/java/com/example/college_space/All_Logins_Activity.java
+++ app/src/main/java/com/example/college_space/All_Logins_Activity.java
@@ -2,9 +2,13 @@ package com.example.college_space;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 
 import com.example.college_space.R;
+import com.example.registration.Registraion;
 
 public class All_Logins_Activity extends AppCompatActivity {
 
@@ -12,5 +16,12 @@ public class All_Logins_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_logins);
+    }
+
+    public void registrationForm(View view)
+    {
+        Intent intent=new Intent(All_Logins_Activity.this, Registraion.class);
+        startActivity(intent);
+
     }
 }
