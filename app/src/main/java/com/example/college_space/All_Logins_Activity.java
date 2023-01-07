60bdd97c05b5d462c885fd17a7edaeae7403b85e
vishwjeert <vrvishwujgare@gmail.com>
2023-01-07 18:23:04 +0530

    added admin app and creted admin dashboard

--- app/src/main/java/com/example/college_space/All_Logins_Activity.java
+++ app/src/main/java/com/example/college_space/All_Logins_Activity.java
@@ -1,13 +1,15 @@
 package com.example.college_space;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.example.registration.Registration;
+import com.example.admin_representation.AdminDashboard;
+
 
 public class All_Logins_Activity extends AppCompatActivity {
 
@@ -22,5 +24,11 @@ public class All_Logins_Activity extends AppCompatActivity {
         Intent intent=new Intent(All_Logins_Activity.this, Registration.class);
         startActivity(intent);
 
+    }
+
+    public void onAdminDash(View view)
+    {
+        Intent intent =new Intent(All_Logins_Activity.this, AdminDashboard.class);
+        startActivity(intent);
     }
 }
